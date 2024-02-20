2ffcb0b5d61ad2c5322ca3e01027126db8d05ffa
simon-geobotica <133072301+simon-geobotica@users.noreply.github.com>
2024-02-20 16:13:16 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt update
 apt upgrade -y
-apt install -y xfce4 wpagui python3-open3d
+apt install -y xfce4 xfce4-terminal wpagui python3-open3d
 apt autoremove
 curl -fsSL https://tailscale.com/install.sh | sh
 tailscale up --ssh
